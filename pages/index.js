b79dba9a7c8423f2276f675c27d7698d60f06345
Martinas Terskin <martinas.terskin@gmail.com>
2023-01-22 20:31:23 -0500

    latest

--- pages/index.js
+++ pages/index.js
@@ -15,30 +15,30 @@ const images = [
   {
     original: "2022/a_IMG.png",
     thumbnail: "2022/a_IMG.png",
-    title: "Yearly tradition",
+    title: "Annual Tradition",
     description:
-      "Martinas started the year with the annual boys ski trip, something he's been doing with friends since high school. Lissa joined for part of the journey, staying in Seattle while the rest of the group continued on to Lake Tahoe.",
+      "Martinas started the year with the annual boys' ski trip. Lissa joined for part of the flight, staying in   Seattle, while the rest of the group continued to Lake Tahoe. ",
   },
   {
     original: "2022/b_IMG.png",
     thumbnail: "2022/b_IMG.png",
     title: "The Lads",
     description:
-      "Lake Tahoe had amazing views, and Martinas hopes to be back there soon.",
+      "Martinas and the guys enjoyed Lake Tahoe's 'Heavenly' views, and took advantage of the powdery slopes.",
   },
   {
     original: "2022/c_IMG.png",
     thumbnail: "2022/c_IMG.png",
     title: "Nailed It",
-    description: "",
+    description: "An iconic photo of our friend, Danny.",
   },
   {
     original: "2022/e_IMG.mov",
     thumbnail: "2022/e_IMG.mov",
-    title: "Escaping the cold",
+    title: "Winter Escape",
     video: true,
     description:
-      "In February, Lissa took a trip to Miami to meet up with friends who flew in from London. It was a fun reunion, and highlights included spotting crocodiles on an airboat tour, seeing all kinds of wildlife in the Everglades and spotting a Portuguese Man-o'-War on South Beach.",
+      "In February, Lissa met her friends, Treeni and Ellie, in Miami. It wasn't the quite the escape from colder (read: DC and London) climates, but the trip was still relaxing (except for maybe the airboat tour) walking around the city, and seeing all kinds of wildlife in the Everglades.",
   },
   {
     original: "2022/d_IMG.mov",
@@ -46,7 +46,7 @@ const images = [
     video: true,
     title: "Beach life",
     description:
-      "Lissa also got to see family in Florida, visiting her Uncle who not only took her to see the house that he and Lissa's dad grew up in, but also drove her by Tiger Wood's house on Jupiter Island.",
+      "Lissa also got to see family in Florida, visiting her Uncle Randy who not only took her to see the house that he and Lissa's dad grew up in, but also drove her by Tiger Wood's house on Jupiter Island.",
   },
   {
     original: "2022/f_IMG.mov",
@@ -54,7 +54,7 @@ const images = [
     video: true,
     title: "Joshua Tree",
     description:
-      "One of Lissa's favorite trips this year was in March, hiking around Joshua Tree and exploring different parts of southern California with a friend.",
+      "One of Lissa's favorite trips this year was in March, hiking around Joshua Tree and exploring different parts of southern California with her friend, Jamie (who flew in from Seattle, so seen here soaking up the sun).",
   },
   {
     original: "2022/g_IMG.png",
@@ -69,7 +69,7 @@ const images = [
     video: true,
     title: "Teleworking in Chicago",
     description:
-      "Having more flexibility to work remotely this year allowed Lissa to visit a friend in Chicago in May, where they worked during the day and had fun the rest of the time biking in the sun, eating Oberweiss ice cream and seeing an orchestral Taylor Swift concert.",
+      "Having more flexibility to work remotely this year allowed Lissa to visit her friend, Amy, in Chicago, where they worked during the day and had fun the rest of the time biking in the sun, eating Oberweiss ice cream and seeing an orchestral Taylor Swift concert.",
   },
   {
     original: "2022/k_IMG.mov",
@@ -77,21 +77,21 @@ const images = [
     video: true,
     title: "More beach trips",
     description:
-      "Lissa started a new job in April, that took her to coastal cities (read: beaches) several times this year. In June, she took a trip in June to Virginia Beach to see these magnificent offshore wind turbines.",
+      "Lissa started a new job in April, that took her to coastal cities and beaches several times this year. In June, she took a trip to Virginia Beach to see these magnificent offshore wind turbines.",
   },
   {
     original: "2022/l_IMG.png",
     thumbnail: "2022/l_IMG.png",
     title: "DC meetup",
     description:
-      "Martinas and Lissa loved seeing more friends in person this year, after having so many virtual meetups to keep in touch.",
+      "We loved seeing more friends in person this year, after having so many virtual meetups to keep in touch. Here we are at a favorite museum with friends Treeni and Luke.",
   },
   {
     original: "2022/m_IMG.png",
     thumbnail: "2022/m_IMG.png",
     title: "Weekend getaway",
     description:
-      "One of the trips Lissa took with friends she had not seen in awhile was to a baby goat farm in North Carolina.",
+      "One of the trips Lissa took with her 'triplet' friends, Ann and Emily, was to a baby goat farm in North Carolina.",
   },
   {
     original: "2022/n_IMG.mov",
@@ -99,90 +99,87 @@ const images = [
     video: true,
     title: "Wedding season begins",
     description:
-      "Martinas and Lissa attended their first wedding of the year in July, celebrating a high school friend who got married in upstate New York. Highlights included two ceremonies, rainbows, raw bar cocktail hour, dancing to a live band and spending a weekend with friends.",
+      "We attended our first wedding of the year in July, celebrating our friend, Karthik, who got married in upstate New York. Highlights included two ceremonies, rainbows, raw bar cocktail hour, dancing to a live band and spending a weekend with friends.",
   },
   {
     original: "2022/o_IMG.png",
     thumbnail: "2022/o_IMG.png",
-    title: "Banff",
-    description: "",
+    title: "Boys Trip",
+    description:
+      "Celebrating a friend, Igor's, bachelor party in Banff was a highlight for Martinas. He can't wait to return to the Canadian Rockies!",
   },
   {
     original: "2022/p_IMG.png",
     thumbnail: "2022/p_IMG.png",
     title: "Tennis with the pros",
-    description: `.`,
+    description: `Lissa's cousin, David, visited the family in August. Everyone got to enjoy Maryland crabs, and dad and Lissa even had time to play some tennis with the visiting pro!`,
   },
   {
-    original: "2022/s_IMG.png",
-    thumbnail: "2022/s_IMG.png",
+    original: "2022/s_IMG.mov",
+    thumbnail: "2022/s_IMG.mov",
+    video: true,
     title: "Горько!",
-    description: " ",
+    description:
+      "We celebrated our friends Igor and Ruslana's wedding in September. Highlights include pre- wedding boys hangout, lots of dancing and laughing and post-wedding late night McNuggets and Big Macs.",
   },
   {
     original: "2022/t_IMG.mov",
     thumbnail: "2022/t_IMG.mov",
     video: true,
-    title: "Harry Potter, Love Bugs and The Weeknd",
+    title: "Harry Potter and Haunted Houses",
     description:
-      "Cheers to 2022! We wish you a healthy and joyful rest of the year, and look forward to safely seeing everyone in the new year! ",
+      "Also in September, Lissa took a trip with Jamie to Florida, where they relaxed on the beach and took a quick trip to Orlando to run around Harry Potter World, and be frightened at the Weeknd's Haunted House.  ",
   },
   {
     original: "2022/u_IMG.png",
     thumbnail: "2022/u_IMG.png",
     title: "Bridesmaids and new friends",
-    description: " ",
+    description:
+      "In October, Lissa was a bridesmaid for her friend Cynthia's wedding. It was a perfectly sunny weekend spent with old and new friends.",
   },
   {
     original: "2022/v_IMG.png",
     thumbnail: "2022/v_IMG.png",
     title: "Auntie and Uncle life",
-    description: " ",
+    description:
+      "Later in October, we stopped in Washington to see family. We were greeted by Lissa's cousin, Kristen's, three adorable kids.",
   },
   {
     original: "2022/w_IMG.png",
     thumbnail: "2022/w_IMG.png",
     title: "Destination: Hawaii",
     description:
-      "One of the highly anticipated trips of this year was for another high school friend's wedding in Hawaii.",
+      "One of the highly anticipated trips of the year was in October to Hawaii, where Martinas was a groomsman for our friend Danny. Highlights include reuniting with friends, Lydia and Jen, water slides and eating all the poke together.",
   },
   {
-    original: "2022/x_IMG.mov",
-    thumbnail: "2022/x_IMG.mov",
-    title: "Sea turtles in the wild",
-    description: " ",
-    video: true,
+    original: "2022/x_IMG.png",
+    thumbnail: "2022/x_IMG.png",
+    title: "Kayaking to the Mokes",
+    description:
+      "One of the best things we did in Hawaii was getting out on the water to kayak to the Mokulua Island bird sanctuaries.",
   },
-  {
-    original: "2022/y_IMG.mov",
-    thumbnail: "2022/y_IMG.mov",
-    title: "Home away from home",
-    description: " ",
-    video: true,
-  },
+
   {
     original: "2022/z_IMG.png",
     thumbnail: "2022/z_IMG.png",
     title: "BLINKs are back",
-    description: " ",
+    description:
+      "Lissa started running again with her BLINKs, John, Julie and Nicole, reuniting for the Turkey Trot 10k on Thanksgiving Day.",
   },
   {
     original: "2022/aa_IMG.png",
     thumbnail: "2022/aa_IMG.png",
     title: "Thankful",
-    description: " ",
+    description:
+      "We are so grateful that we live close to family, and it is becoming an annual tradition to celebrate Thanksgiving with both sides of our families together.   ",
   },
-  {
-    original: "2022/bb_IMG.png",
-    thumbnail: "2022/bb_IMG.png",
-    title: "Staying local for the holidays",
-    description: " ",
-  },
+
   {
     original: "2022/cc_IMG.png",
     thumbnail: "2022/cc_IMG.png",
     title: "Christmas walk",
-    description: " ",
+    description:
+      "We ended the year with some freezing temperatures, but also a chance to enjoy a winter walk with all the siblings + Mike, Cory and Boogie.",
   },
 ];
 
